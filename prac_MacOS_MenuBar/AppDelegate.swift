4b38d1fca6e16655fb96d0ef42fca0b88730dddb
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2022-10-27 17:13:36 +0900

    Initial Commit

---
+++ prac_MacOS_MenuBar/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  prac_MacOS_MenuBar
+//
+//  Created by 임준화 on 2022/10/27.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ prac_MacOS_MenuBar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  prac_MacOS_MenuBar
+//
+//  Created by 임준화 on 2022/10/27.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
