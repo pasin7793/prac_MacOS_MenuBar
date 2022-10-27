c1b9c966d09051d9db889af1c12b0dac6a59d8b8
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2022-10-27 18:05:36 +0900

    ViewController

--- prac_MacOS_MenuBar/ViewController.swift
+++ prac_MacOS_MenuBar/ViewController.swift
@@ -9,12 +9,41 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    
+    var changeStatusButton: NSButton = {
+        let btn = NSButton(title: "change to random emoji", target: self, action: #selector(changeStatusButton(_:)))
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
+    
+    override func loadView() {
+        super.loadView()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.view.addSubview(self.changeStatusButton)
+        self.setupContraints()
     }
-
+    
+    func setupContraints(){
+        NSLayoutConstraint.activate([
+            self.changeStatusButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.changeStatusButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+        ])
+    }
+    
+    @objc func changeStatusButton(_ sender: NSButton){
+        
+        let emojis = ["🇰🇷","🇨🇳","🇰🇵","🇺🇸"]
+        let randomIndex = Int.random(in: 0..<emojis.count)
+        
+        if let delegate = NSApplication.shared.delegate as? AppDelegate{
+            delegate.statusItem.button?.changeStatus(emojiString: emojis[randomIndex])
+        }
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
