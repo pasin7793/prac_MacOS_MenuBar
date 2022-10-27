3b044e53ed4bd85b3739e75b2debec06a4dbfb43
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2022-10-27 17:56:10 +0900

    AppDelegate

--- prac_MacOS_MenuBar/AppDelegate.swift
+++ prac_MacOS_MenuBar/AppDelegate.swift
@@ -1,20 +1,66 @@
-//
-//  AppDelegate.swift
-//  prac_MacOS_MenuBar
-//
-//  Created by 임준화 on 2022/10/27.
-//
+
 
 import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    var statusItem: NSStatusItem = {
+        NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength )
+    }()
+    
+    var popover: NSPopover = {
+        let popover = NSPopover()
+        popover.behavior = .transient
+        return popover
+    }()
     
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let storyboard = NSStoryboard(name: "Main", bundle: Bundle.main)
+        guard let windowController = storyboard.instantiateController(withIdentifier: "MainViewController") as? NSWindowController else {
+            return
+        }
+        
+        let contentViewController = windowController.contentViewController
+        
+        let btn = self.statusItem.button
+        btn?.action = #selector(statusItemClicked(_:))
+        btn?.sendAction(on: [.leftMouseDown, .rightMouseDown])
+        btn?.changeStatus(emojiString: "🇯🇵")
+        
+        self.popover.contentViewController = contentViewController
+    }
+    
+    @objc func statusItemClicked(_ sender: NSStatusBarButton){
+        let isRightClickEvent = NSApp.currentEvent?.isRightClick ?? false
+        
+        if self.popover.isShown{
+            self.popover.close()
+        }
+        
+        if !isRightClickEvent {
+            self.popover.show(relativeTo: sender.bounds, of: sender, preferredEdge: .minY)
+            return
+        }
+        self.showMenu()
+    }
+    
+    func showMenu(){
+        var location = NSEvent.mouseLocation
+        location.y = NSScreen.main!.frame.height - NSStatusBar.system.thickness - 10
+        
+        let contentRect = NSRect(origin: location, size: CGSize(width: 0, height: 0))
+        
+        let tmpWindow = NSWindow(contentRect: contentRect, styleMask: .borderless, backing: .buffered, defer: false)
+        tmpWindow.isReleasedWhenClosed = true
+        
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Info", action: nil, keyEquivalent: " "))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        
+        menu.popUp(positioning: nil, at: .zero, in: tmpWindow.contentView)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -28,3 +74,37 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 }
 
+extension String {
+    func emojiToImage(with fontSize: CGFloat) -> NSImage? {
+        let size = CGSize(width: fontSize, height: fontSize)
+        let image = NSImage(size: size, flipped: true, drawingHandler: { dstRect in
+            
+            let textOrigin = CGPoint(x: 0, y: 0)
+            
+            let rect = CGRect(origin: textOrigin, size: dstRect.size)
+            
+            let textFont = NSFont.systemFont(ofSize: fontSize)
+            let attributes = [NSAttributedString.Key.font: textFont,]
+            
+            self.draw(in: rect, withAttributes: attributes)
+            return true
+        })
+        
+        return image
+    }
+}
+
+extension NSStatusBarButton{
+    func changeStatus(emojiString: String){
+        guard let statusBtnImage = emojiString.emojiToImage(with: 18) else {
+            return
+        }
+        self.image = statusBtnImage
+    }
+}
+
+extension NSEvent{
+    var isRightClick: Bool{
+        self.type == .rightMouseDown
+    }
+}
